16d064d8d8114443f05b1b6ccf46e88752fca871
Sanjay Bhangar <sanjaybhangar@gmail.com>
2019-06-17 20:10:17 +0530

    upload predictions to api

--- ml_enabler/commands/upload_predictions.py
+++ ml_enabler/commands/upload_predictions.py
@@ -1,10 +1,25 @@
 import click
 import json
+from ml_enabler.utils.api import get_model_id, post_prediction, post_prediction_tiles
 
-@click.command('aggregate_predictions', short_help='Aggregate predictions to lower zoom levels and add ancillary data')
+@click.command('upload_predictions', short_help='Upload predictions JSON to the ML Enabler API')
 @click.option('--infile', help='Filename to read predictions JSON from', type=click.File('r'))
-@click.option('--api-url', help='ML Enabler API URL', default='http://localhost:8000/')
+@click.option('--api-url', help='ML Enabler API URL', default='http://localhost:5000/')
 @click.pass_context
 def upload(ctx, infile, api_url):
+    data = json.load(infile)
+    metadata = data['metadata']
+    predictions = data['predictions']
+    model_name = metadata['model_name']
+    model_id = get_model_id(api_url, model_name)
+    bbox = metadata['bbox']
+    version = metadata['version']
+    prediction_id = post_prediction(api_url, model_id, version, bbox)
+    for p in predictions:
+        p['prediction_id'] = prediction_id
+    post_prediction_tiles(api_url, prediction_id, predictions)
+    print(f'uploaded predictions to model_id={model_id}, prediction_id={prediction_id}')
 
+
+    
     
---
+++ ml_enabler/utils/api.py
@@ -0,0 +1,42 @@
+import requests
+from os.path import join
+import json
+
+def get_model_id(api_url, model_name):
+    url = f'{api_url}/model/all'
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception('Failed to fetch model id for model name')
+    data = response.json()
+    for model in data:
+        if model['name'] == model_name:
+            return model['modelId']
+    raise Exception('Model with that name does not exist')
+
+def post_prediction(api_url, model_id, version, bbox):
+    url = f'{api_url}/model/{model_id}/prediction'
+    data = {
+        'version': f'{version}.0.0',
+        'modelId': model_id,
+        'bbox': bbox.split(','),
+        'tileZoom': 14 # FIXME:
+    }
+    print(data)
+    response = requests.post(url, json=data)
+    if response.status_code != 200:
+        raise Exception('could not POST Prediction')
+    response_data = response.json()
+    return response_data['prediction_id']
+
+def post_prediction_tiles(api_url, prediction_id, predictions):
+    url = f'{api_url}/model/prediction/{prediction_id}/tiles'
+    data = {
+        'predictionId': prediction_id,
+        'predictions': predictions
+    }
+    response = requests.post(url, json=data)
+    import pdb;pdb.set_trace()
+    if response.status_code != 200:
+        raise Exception('Error posting tiles for prediction')
+    return True
+
