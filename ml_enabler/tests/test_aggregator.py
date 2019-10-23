7ed6c7706b6eca647ef964e029dfe6a3d4ecdf33
Ming Gao <gaoxm@fb.com>
2019-10-23 19:18:19 -0400

    add tests for Map With AI road stats predictor and aggregator

--- ml_enabler/tests/test_aggregator.py
+++ ml_enabler/tests/test_aggregator.py
@@ -55,3 +55,42 @@ def test_building_aggregator():
     assert(result.exit_code) == 0
     assert(expected_results == current_results)
     os.remove('/tmp/aggregator.json')
+
+
+def test_mapwithai_road_stats_aggregator():
+
+    def overpass_callback():
+        overpass_response = open('ml_enabler/tests/fixtures/overpass_response_road.xml').read()
+        return overpass_response
+
+    server = MockServer(port=1234)
+    server.start()
+    server.add_callback_response('/api/interpreter', overpass_callback)
+    infile = 'ml_enabler/tests/fixtures/mapwithai_road_stats_fetch_expected.json'
+    expected_results = json.load(open('ml_enabler/tests/fixtures/mapwithai_road_stats_aggregator_expected.json'))
+    outfile = '/tmp/aggregator.json'
+
+    runner = CliRunner()
+    result = runner.invoke(
+        main_group,
+        [
+            'aggregate_predictions',
+            '--name',
+            'mapwithai_road_stats',
+            '--zoom',
+            15,
+            '--infile',
+            infile,
+            '--outfile',
+            outfile,
+            '--overpass-url',
+            'http://localhost:1234/api/interpreter'
+        ]
+    )
+    current_results = json.load(open(outfile))
+    expected_results['predictions'] = sorted(expected_results['predictions'], key=lambda p: p['quadkey'])
+    current_results['predictions'] = sorted(current_results['predictions'], key=lambda p: p['quadkey'])
+    server.shutdown_server()
+    assert(result.exit_code == 0)
+    assert(expected_results == current_results)
+    os.remove('/tmp/aggregator.json')
